c9b95cb31e12d611a0a9a16ab1bf49ef837867b8
AndroidLollipop <obfuscated00143241131643657478@gmail.com>
2020-12-22 11:19:31 +0800

    update tab label

--- src/index.js
+++ src/index.js
@@ -401,7 +401,7 @@ const Tabs = ({children, selTab, setSelTab}) => {
 const Tab = ({label, onClick, active, removable, removeCallback}) => {
   return (
     <span>
-      <Material.Tab label={" "+label+" "} onClick={onClick}/>
+      <Material.Tab label={label} onClick={onClick}/>
       {removable ? (<Material.IconButton size="small" onClick={removeCallback}><Icons.Close style={{fill: "red"}}/></Material.IconButton>) : undefined}
     </span>
   )
